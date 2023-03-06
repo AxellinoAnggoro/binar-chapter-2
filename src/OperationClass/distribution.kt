6e7768904986ea96a3cde330f957ca8bfc3f8cf5
Hafidz Putra Herlyansyah <apiiettrecomen1@gmail.com>
2023-03-06 23:09:54 +0700

    commit

--- src/OperationClass/distribution.kt
+++ src/OperationClass/distribution.kt
@@ -1,11 +1,11 @@
 package OperationClass
 
 class distribution {
-    fun bagi(angka1: Int, angka2: Int): Int {
-        if (angka2 == 0) {
-            println("Tidak bisa membagi dengan angka nol")
+    fun bagi(angka1: Double, angka2: Double): String {
+        if (angka2 == 0.0) {
+            return "Tidak bisa membagi dengan angka nol"
         }
-        return angka1 / angka2
+        return (angka1 / angka2).toString()
 
     }
 }
--- src/main.kt
+++ src/main.kt
@@ -1,23 +1,31 @@
+import OperationClass.Multiply
 import OperationClass.Subtraction
 import OperationClass.Sum
+import OperationClass.distribution
 
 fun main() {
     println("Pilih operasi dibawah ini: ")
     println("1. Pertambahan (+)")
     println("2. Pengurangan (-)")
     println("3. Pembagian (/)")
-    println("4. Modulo (%)")
-    println("5. Perkalian (*)")
+    println("4. Perkalian (*)")
     print("Pilih Operasi: ")
     val operasi = readln().toInt()
-    print("Masukan Angka ke-1: ")
-    val number1 = readln().toDouble()
-    print("Masukan Angka ke-2: ")
-    val number2 = readln().toDouble()
 
-
-    val hasil = when(operasi){
-        1 -> Sum().sumNumber(number1, number2)
-        2 -> Subtraction().subtractionNumber(number1, number2)
+    if(operasi > 4){
+        println("Operasi yang anda pilih tidak ada!!!")
+    }else{
+        print("Masukan Angka ke-1: ")
+        val number1 = readln().toDouble()
+        print("Masukan Angka ke-2: ")
+        val number2 = readln().toDouble()
+        val hasil = when(operasi){
+            1 -> Sum().sumNumber(number1, number2)
+            2 -> Subtraction().subtractionNumber(number1, number2)
+            3 -> distribution().bagi(number1, number2)
+            4 -> Multiply().multiplyNum(number1, number2)
+            else -> "Daftar operasi yang anda pilih tidak ada!!!"
+        }
+        println("Hasil : $hasil")
     }
 }
