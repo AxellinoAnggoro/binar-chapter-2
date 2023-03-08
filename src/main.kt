8eb88d2382364c1fbaa3b71a8bc79b47600f2776
ilhamokta13 <ilhamok8@gmail.com>
2023-03-08 14:23:08 +0700

    Merge remote-tracking branch 'origin/master'

--- src/main.kt
+++ src/main.kt
@@ -4,28 +4,20 @@ import OperationClass.Sum
 import OperationClass.distribution
 
 fun main() {
-    println("Pilih operasi dibawah ini: ")
-    println("1. Pertambahan (+)")
-    println("2. Pengurangan (-)")
-    println("3. Pembagian (/)")
-    println("4. Perkalian (*)")
-    print("Pilih Operasi: ")
-    val operasi = readln().toInt()
 
-    if(operasi > 4){
-        println("Operasi yang anda pilih tidak ada!!!")
-    }else{
-        print("Masukan Angka ke-1: ")
-        val number1 = readln().toDouble()
-        print("Masukan Angka ke-2: ")
-        val number2 = readln().toDouble()
-        val hasil = when(operasi){
-            1 -> Sum().sumNumber(number1, number2)
-            2 -> Subtraction().subtractionNumber(number1, number2)
-            3 -> distribution().bagi(number1, number2)
-            4 -> Multiply().multiplyNum(number1, number2)
-            else -> "Daftar operasi yang anda pilih tidak ada!!!"
-        }
-        println("Hasil : $hasil")
+    print("Masukkan angka pertama: ")
+    val angkaPertama = readLine()!!.toDouble()
+    print("Masukkan angka kedua: ")
+    val angkaKedua = readLine()!!.toDouble()
+    print("Pilih operator (+, -, *, /): ")
+    val operator = readLine()
+
+    val hasil = when (operator) {
+        "+" -> Sum().sumNumber(angkaPertama, angkaKedua)
+        "-" -> Subtraction().subtractionNumber(angkaPertama, angkaKedua)
+        "/"-> distribution().bagi(angkaPertama, angkaKedua)
+        "*"-> Multiply().multiplyNum(angkaPertama, angkaKedua)
+        else -> 0.0
     }
+    println("Hasil : $hasil")
 }
